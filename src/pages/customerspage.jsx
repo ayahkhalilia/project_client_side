2ea4b3c5ca2a6124fcc3f8292be1e8eb035fb805
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2025-01-19 14:49:41 +0200

    created the customers page and added the navigation bar

--- src/pages/customerspage.jsx
+++ src/pages/customerspage.jsx
@@ -9,30 +9,67 @@ import SearchBar from '../components/searchbar.jsx';
 import '../index.css';
 
 const CustomersPage = () => {
+        const handleSearch = (query) => {
+            const filteredResults = data.filter((item) =>
+                item.toLowerCase().includes(query.toLowerCase())
+            );
+            setResults(filteredResults); // Update results state
+        };
+        const userName="jayjay";//this is for test it need it from server
+        const customers=["Alice", "Bob", "Charlie", "Diana"];
     return(
         <div className='nav-bar'>
-            <h3>Customers page</h3>
-            <h3><Link to="/home" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <div className='bar-rec'>
+              <h3><Link to="/home">
                     <IoHomeOutline /> Home
-                </Link> 
-            </h3>
-            <h3><Link to="/customers" style={{ textDecoration: 'none', color: 'inherit' }}>
+                  </Link> 
+              </h3>
+              <h3><Link to="/customers">
                     <LuUsersRound /> Customers
-                </Link>
-            </h3>
-            <h3><Link to="/book-requests" style={{ textDecoration: 'none', color: 'inherit' }}>
+                  </Link>
+              </h3>
+              <h3><Link to="/book-requests">
                     <RiBookShelfLine /> Book Requests
-                </Link>
-            </h3>
-            <h3><Link to="/book-donations" style={{ textDecoration: 'none', color: 'inherit' }}>
+                  </Link>
+              </h3>
+              <h3><Link to="/book-donations">
                     <BiDonateHeart /> Book Donations
-                </Link>
-            </h3>
-            <h3><Link to="/room-booking" style={{ textDecoration: 'none', color: 'inherit' }}>
+                  </Link>
+              </h3>
+              <h3><Link to="/room-booking">
                     <MdOutlineDoorFront /> Room Booking
-                </Link>
-            </h3>
-            <div id='setting'><IoSettingsOutline /></div>
+                  </Link>
+              </h3>
+            <div className='setting'><IoSettingsOutline /></div>
+            </div>
+
+            
+            <div className='content'>
+                <header className='header'>
+                    <h3 className='homepage'>Customers</h3>        
+                    
+                    <div className='user-info'>
+                        <img
+                            src='#'
+                            className='profile-pic'
+                        />
+                        <span>Hi,{userName}</span>
+                    </div> 
+                </header>
+                <div className='search-bar'>
+                        <SearchBar onSearch={handleSearch} />
+                </div> 
+                <div className='customer-list'>
+                     <h3>Customer list</h3>
+                     <ul>
+                       {customers.map((customer,index)=>(
+                        <li key={index}>{customer}</li>
+                       ))}
+                     </ul>
+                </div>
+            </div>
+            
+            
         </div>
     );
 };
