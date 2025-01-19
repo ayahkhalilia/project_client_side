660f36158afac4945b85ff27c8d3862e180de933
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2025-01-19 14:47:42 +0200

    created the donations page and added the navigation bar

--- src/pages/bookdonationspage.jsx
+++ src/pages/bookdonationspage.jsx
@@ -5,34 +5,63 @@ import { RiBookShelfLine } from "react-icons/ri";
 import { BiDonateHeart } from "react-icons/bi";
 import { MdOutlineDoorFront } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import SearchBar from '../components/searchbar.jsx';
 import '../index.css';
 
 const BookDonationsPage = () => {
-    return (
+    const handleSearch = (query) => {
+        const filteredResults = data.filter((item) =>
+            item.toLowerCase().includes(query.toLowerCase())
+        );
+        setResults(filteredResults); // Update results state
+    };
+    const userName="jayjay";//this is for test it need it from server
+
+    return(
         <div className='nav-bar'>
-                    <h3>Book donations page</h3>
-                    <h3><Link to="/home" style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <IoHomeOutline /> Home
-                        </Link> 
-                    </h3>
-                    <h3><Link to="/customers" style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <LuUsersRound /> Customers
-                        </Link>
-                    </h3>
-                    <h3><Link to="/book-requests" style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <RiBookShelfLine /> Book Requests
-                        </Link>
-                    </h3>
-                    <h3><Link to="/book-donations" style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <BiDonateHeart /> Book Donations
-                        </Link>
-                    </h3>
-                    <h3><Link to="/room-booking" style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <MdOutlineDoorFront /> Room Booking
-                        </Link>
-                    </h3>
-                    <div id='setting'><IoSettingsOutline /></div>
-                </div>
+            <div className='bar-rec'>
+              <h3><Link to="/home">
+                    <IoHomeOutline /> Home
+                  </Link> 
+              </h3>
+              <h3><Link to="/customers">
+                    <LuUsersRound /> Customers
+                  </Link>
+              </h3>
+              <h3><Link to="/book-requests">
+                    <RiBookShelfLine /> Book Requests
+                  </Link>
+              </h3>
+              <h3><Link to="/book-donations">
+                    <BiDonateHeart /> Book Donations
+                  </Link>
+              </h3>
+              <h3><Link to="/room-booking">
+                    <MdOutlineDoorFront /> Room Booking
+                  </Link>
+              </h3>
+            <div className='setting'><IoSettingsOutline /></div>
+            </div>
+
+            
+            <div className='content'>
+                <header className='header'>
+                    <h3 className='homepage'>Book donations</h3>        
+                    
+                    <div className='user-info'>
+                        <img
+                            src='#'
+                            className='profile-pic'
+                        />
+                        <span>Hi,{userName}</span>
+                    </div> 
+                </header>
+                <div className='search-bar'>
+                        <SearchBar onSearch={handleSearch} />
+                </div> 
+            </div>
+ 
+        </div>
     );
 };
 
