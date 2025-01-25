a819426b784fd3756c859b656d78b5561c3351e3
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2025-01-26 00:54:21 +0200

    trying to fix the fetch books lag

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+// Base URL of your backend
 const API = axios.create({
-  baseURL: 'https://rebook-backend-ldmy.onrender.com', // Backend URL
-  withCredentials: true, // Include credentials if needed
+    baseURL: 'https://rebook-backend-ldmy.onrender.com', // Replace with your actual backend URL
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 export default API;
-
